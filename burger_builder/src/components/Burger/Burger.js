66475a05fa1d1805772a640db63fa7fe2e58d231
BonesTheGhost <bonestheghost@gmail.com>
2020-01-14 18:41:38 -0600

    [8] UPDATE:: Added 'BuildControls Sub-structure and the singular 'BuildControl' as well as relevant stylesheets. Will be adding the ability to output multiple build controls.

--- burger_builder/src/components/Burger/Burger.js
+++ burger_builder/src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient key={igKey + i} type={igKey} />
-        }); // [,]
-    });
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            }); // [,]w
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    //^Reduce is a built in array function that allows us to transform an array into something else. Takes 2 arguments.
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please Start Adding Ingredients!</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
