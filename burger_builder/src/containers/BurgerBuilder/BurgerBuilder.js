e7dd739c8b2130af38e72b168cdc63a4bba488ed
BonesTheGhost <bonestheghost@gmail.com>
2020-01-14 19:01:35 -0600

    [9] UPDATE:: Added the Build Controls below the burger visual to make the app more user friendly. Will be attempting to hook up state to the Build Controls and remedying the blue background.

--- burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Aux from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     //constructor(props) {
@@ -9,10 +10,10 @@ class BurgerBuilder extends Component {
     //}
     state= {
         ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
         }
     }
 
@@ -20,7 +21,7 @@ class BurgerBuilder extends Component {
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls />  
             </Aux>
         );
     }
